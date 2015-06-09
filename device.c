00aff789a9ddf616974536f33deb95dc3c809e74
Christopher Hewitt <hewittc@users.noreply.github.com>
2015-06-09 11:31:53 -0400

    Remove UHD stub.

--- device.c
+++ device.c
@@ -1,10 +1,8 @@
 #include "common.h"
 #include "device.h"
 #include "device_hackrf.h"
-#include "device_uhd.h"
 
 device_iface_t devices[] = {
 	{ NULL, NULL },					/* DUMMY */
 	{ device_hackrf_config, device_hackrf_xfer },	/* HACKRF */
-	{ device_uhd_config, device_uhd_xfer }		/* UHD */
 };
