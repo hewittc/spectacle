a82318adc1dd44b19b6a6ed9a3537f26e0cf2609
Christopher Hewitt <hewittc@users.noreply.github.com>
2016-04-19 11:36:59 -0400

    Write data to file for testing.

--- main.c
+++ main.c
@@ -10,15 +10,34 @@ int main(int argc, char **argv)
 	if (dev) {
 		dev_iface->setup(dev, 92e6, 20e6);
 		dev_iqfile_open(dev, "./pager.iq", false);
+
 		size_t bins = 512;
-		dev_iface->rx(dev, bins);
-		printf_cbuffer(dev->buffer, bins);
 
-		printf("\n");
+		char filename[255] = {0};
+		for (size_t j = 0; j < 10; j++) {
+			snprintf(filename, sizeof(filename), "results-%zu.txt", j);
+			FILE *fp = fopen(filename, "w");
 
-		complex float *out = calloc(bins, sizeof(complex float));
-		apply_window(dev->buffer, out, bins, HANN);
-		printf_cbuffer(out, bins);
+			dev_iface->rx(dev, bins);
+			//printf_cbuffer(dev->buffer, bins);
+			//printf("\n");
+
+			complex float *out = calloc(bins, sizeof(complex float));
+			apply_window(dev->buffer, out, bins, HANN);
+			//printf_cbuffer(out, bins);
+			//printf("\n");
+
+			fft(out, out, bins, 0);
+			//printf_cbuffer(out, bins);
+			//printf("\n");
+
+			for (size_t i = 0; i < bins; i++) {
+				fprintf(fp, "%zu %f\n", i, 20.0 * log10f(cabsf(out[i])));
+			}
+
+			fclose(fp);
+		}
+
 		dev_iqfile_close(dev);
 		dev_iface->destroy(dev);
 	}
